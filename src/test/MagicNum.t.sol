2dc380f9c481a31ca0e30db65e363d1751027e20
Ashish Binu <ashishbinu90@gmail.com>
2023-07-08 18:46:00 +0530

    feat: MagicNum hack

--- src/test/MagicNum.t.sol
+++ src/test/MagicNum.t.sol
@@ -4,6 +4,7 @@ import "ds-test/test.sol";
 import "../MagicNum/MagicNumFactory.sol";
 import "../Ethernaut.sol";
 import "./utils/vm.sol";
+import "forge-std/console.sol";
 
 contract MagicNumTest is DSTest {
     Vm vm = Vm(address(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D));
@@ -13,6 +14,8 @@ contract MagicNumTest is DSTest {
         // Setup instance of the Ethernaut contract
         ethernaut = new Ethernaut();
     }
+
+    event log_bytes_(bytes32 value);
 
     function testMagicNum() public {
         /////////////////
@@ -25,11 +28,34 @@ contract MagicNumTest is DSTest {
         address levelAddress = ethernaut.createLevelInstance(magicNumFactory);
         MagicNum ethernautMagicNum = MagicNum(payable(levelAddress));
 
-
         //////////////////
         // LEVEL ATTACK //
         //////////////////
 
+        // INIT CODE
+        // PUSH1 0x0A
+        // PUSH1 0x0C
+        // PUSH1 0x00
+        // CODECOPY
+        // PUSH1 0x0A
+        // PUSH1 0x00
+        // RETURN
+
+        // RUNTIME CODE
+        // PUSH1 0x2a
+        // PUSH1 0x00
+        // MSTORE
+        // PUSH1 0x20
+        // PUSH1 0x00
+        // RETURN
+        bytes memory bytecode = hex"600a600c600039600a6000f3602a60005260206000f3";
+        //                          --------init code------|---runtime code-----
+        address contractAddress;
+        assembly {
+            contractAddress := create(0, add(bytecode, 0x20), mload(bytecode))
+            if iszero(extcodesize(contractAddress)) { revert(0, 0) }
+        }
+        ethernautMagicNum.setSolver(contractAddress);
 
         //////////////////////
         // LEVEL SUBMISSION //
